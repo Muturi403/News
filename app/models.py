bed5a487d9e3dad08cabed4a7177b38395427ac7
Muturi403 <faith.muturi@student.moringaschool.com>
2021-08-08 12:04:26 +0300

    defined the articles object

--- app/models.py
+++ app/models.py
@@ -1,10 +1,25 @@
 class Source:
-    """ 
+    ''' 
     Sources class to define the news source objects 
-    """
+    '''
     def __init__(self, id, name, description, url, category):
         self.id = id
         self.name = name
         self.description = description
         self.url = url
         self.category = category
+        
+class Article:
+    '''
+    Articles class to define the articles object
+    '''
+    def _init_(self, author, title, description, pathtoImage, url, datePublished):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.pathToImage = pathToImage
+        self.url = url
+        self.datePublished = datePublished
+    
+    def save_article(self):
+        article.all_articles.append(self)  
