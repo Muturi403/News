25ce349a05a34747607213dc984f036936a60861
Muturi403 <faith.muturi@student.moringaschool.com>
2021-08-08 11:54:37 +0300

    defined news source objects

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class Source:
+    """ 
+    Sources class to define the news source objects 
+    """
+    def __init__(self, id, name, description, url, category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
